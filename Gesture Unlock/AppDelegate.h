a6e724726f529468a8a13cb4a12b29f4e675fb92
Phoenix <holaux@gmail.com>
2016-10-14 15:07:36 +0800

    Initial Commit

---
+++ Gesture Unlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gesture Unlock
+//
+//  Created by Phoenix on 10/14/16.
+//  Copyright © 2016 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gesture Unlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gesture Unlock
+//
+//  Created by Phoenix on 10/14/16.
+//  Copyright © 2016 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
